2f9758ecb987ff4f2b7a26198204c39695c511ff
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-17 16:33:49 -0800

    added stuff for recall info

--- app.py
+++ app.py
@@ -4,10 +4,30 @@ from bottle import run, route
 
 @route('/')
 def index():
-    url = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=json'
-    r = requests.get(url).json()
+    # url = 'https://vpic.nhtsa.dot.gov/api/vehicles/GetModelsForMakeId/440?format=json'
+    # r = requests.get(url).json()
     # return r['Results'][0]['Model_Name']
-    return r
+    # return r
+
+    year = '2009'
+    make = 'honda'
+    model = 'accord'
+    # test call for recalls
+    url = 'https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/'
+    url += year
+    url += '/make/'
+    url += make
+    url += '/model/'
+    url += model
+    url += '?format=json'
+
+    results = []
+    r = requests.get(url).json()
+
+    for result in r['Results']:
+        results.append(result['NHTSACampaignNumber'] + '  ')
+
+    return results
 
 if __name__ == '__main__':
     run(debug=True, reloader=True)
