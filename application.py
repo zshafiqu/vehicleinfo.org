ff659c5ed4b00f627fe94b725cfe7cc424075a51
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-18 16:43:08 -0800

    template is rendering minus the icons

--- application.py
+++ application.py
@@ -1,10 +1,11 @@
-from flask import Flask
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return '<h1> On home page </h1>'
+    # return '<h1> On home page </h1>'
+    return render_template('landingpage.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
