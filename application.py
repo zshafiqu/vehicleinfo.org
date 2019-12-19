5a94524591dc7f2cd142917d1da5420cc88e6c37
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-18 17:41:39 -0800

    added getRecalls function, renamed landingpage to dashboard

--- application.py
+++ application.py
@@ -1,6 +1,24 @@
 from flask import Flask, render_template
+import requests, json
 
+# Create flask app
 app = Flask(__name__)
+
+# Make an API call to the NHTSA page for recall Information
+def getRecalls(year, make, model):
+    # Build URL for call
+    url = 'https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/'
+    url += year
+    url += '/make/'
+    url += make
+    url += '/model/'
+    url += model
+    url += '?format=json'
+
+    # Make request
+    r = requests.get(url).json()
+
+    return r
 
 @app.route('/')
 def index():
@@ -10,7 +28,8 @@ def index():
 @app.route('/random')
 def random():
     # return '<h1> On home page </h1>'
-    return render_template('random.html')
+    # return render_template('random.html')
+    return getRecalls('2007', 'Honda', 'Civic')
 
 
 if __name__ == '__main__':
