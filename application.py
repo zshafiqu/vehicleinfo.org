d409b630f53194dc55d9100274be6f646e3272d2
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-19 14:42:46 -0800

    updated process route, currently working on scraping for edmunds images

--- application.py
+++ application.py
@@ -1,23 +1,26 @@
 from flask import Flask, render_template, request
+from bs4 import BeautifulSoup
 import requests, json
 # ----------------------
 # Create flask app
 app = Flask(__name__)
 # ----------------------
+def scrapeEdmunds(year, make, model):
+    # URL to scrape from
+    url = 'https://www.edmunds.com/'+make+'/'+model+'/'+year+'/review/'
+    # get html
+    source = requests.get(url).text
+    soup = BeautifulSoup(source, 'lxml')
+
+    imgSource = soup.find()
+    return ''
+# ----------------------
 # Make an API call to the NHTSA page for recall Information
 def getRecalls(year, make, model):
-    # Build URL for call
-    url = 'https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/'
-    url += year
-    url += '/make/'
-    url += make
-    url += '/model/'
-    url += model
-    url += '?format=json'
-
+    # Build URL for call to JSON response
+    url = 'https://one.nhtsa.gov/webapi/api/Recalls/vehicle/modelyear/'+year+'/make/'+make+'/model/'+model+'?format=json'
     # Make request
     items = requests.get(url).json()
-    results = []
 
     return items
 # ----------------------
@@ -25,9 +28,12 @@ def getRecalls(year, make, model):
 def process():
     query = request.form['searchparameter']
     parameter = query.split()
+    year = parameter[0]
+    make = parameter[1]
+    model = parameter[2]
 
-    recalls = getRecalls(parameter[0], parameter[1], parameter[2])
-    return render_template('dashboard.html', recalls=recalls, parameter=parameter)
+    recalls = getRecalls(year, make, model)
+    return render_template('dashboard.html', recalls=recalls)
 # ----------------------
 @app.route('/')
 def index():
