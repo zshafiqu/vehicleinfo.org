371e769aadfce49772e6bf87fd78be90affaa2ab
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-19 16:32:09 -0800

    scraping service update

--- scrapingservices/app.py
+++ scrapingservices/app.py
@@ -4,23 +4,29 @@ import requests, json
 # Script to scrape images for each vehicle in our list
 # ----------------------
 def scrapeEdmunds(year, make, model):
-    URL to scrape from
+    # URL to scrape from
     url = 'https://www.edmunds.com/'+make+'/'+model+'/'+year+'/review/'
     # url = 'https://www.edmunds.com/lexus/es-300/2001/review/'
+
     # required to emulate broswer user agent
     headers = {
         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36',
         'Origin': 'http://example.com',
         'Referer': 'http://example.com/some_page'
         }
+
     # get html
     source = requests.get(url, headers=headers).text
     soup = BeautifulSoup(source, 'lxml')
     imgSource = soup.findAll("img", {"class":"w-100"})
+
+    # Run Check
     if len(imgSource) is 0:
         imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
     else:
         imgSource = imgSource[0]['src']
+
+    # imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
     print(imgSource)
-    imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
     return imgSource
+# ----------------------
