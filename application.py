b694a7577c82b11bca54b59f460c22b8f8f3de7b
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-19 16:01:31 -0800

    omitting the edmunds scraper for now

--- application.py
+++ application.py
@@ -13,20 +13,23 @@ app = Flask(__name__)
 def scrapeEdmunds(year, make, model):
     # URL to scrape from
     # url = 'https://www.edmunds.com/'+make+'/'+model+'/'+year+'/review/'
-    url = 'https://www.edmunds.com/lexus/es-300/2001/review/'
-    # required to emulate broswer user agent
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36',
-        'Origin': 'http://example.com',
-        'Referer': 'http://example.com/some_page'
-        }
-    # get html
-    source = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(source, 'lxml')
-    imgSource = soup.findAll("img", {"class":"w-100"})
-    imgSource = imgSource[0]['src']
-    print(imgSource)
-
+    # # url = 'https://www.edmunds.com/lexus/es-300/2001/review/'
+    # # required to emulate broswer user agent
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36',
+    #     'Origin': 'http://example.com',
+    #     'Referer': 'http://example.com/some_page'
+    #     }
+    # # get html
+    # source = requests.get(url, headers=headers).text
+    # soup = BeautifulSoup(source, 'lxml')
+    # imgSource = soup.findAll("img", {"class":"w-100"})
+    # if len(imgSource) is 0:
+    #     imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
+    # else:
+    #     imgSource = imgSource[0]['src']
+    # print(imgSource)
+    imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
     return imgSource
 # ----------------------
 # Make an API call to the NHTSA page for recall Information
