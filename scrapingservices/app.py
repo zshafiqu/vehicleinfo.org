ed4c036b7267e5083e881710e6246d3f52d78eba
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-20 23:24:38 -0800

    jk now staged for testing

--- scrapingservices/app.py
+++ scrapingservices/app.py
@@ -1,46 +1,45 @@
 from bs4 import BeautifulSoup, SoupStrainer
-from urllib import urlopen
 import requests, json, os, mysql.connector, csv, ast, random
 # Script to scrape images for each vehicle in our list
 ''' The goal of this script is to store the source for each image in our database '''
 
-username = os.environ.get('DB_USER')
-dbname = os.environ.get('DB_NAME')
-passwd = os.environ.get('DB_PASS')
-server = os.environ.get('DB_SERVER')
-port = os.environ.get('DB_PORT')
+# username = os.environ.get('DB_USER')
+# dbname = os.environ.get('DB_NAME')
+# passwd = os.environ.get('DB_PASS')
+# server = os.environ.get('DB_SERVER')
+# port = os.environ.get('DB_PORT')
 
 # mydb = mysql.connector.connect(host=server, port=port, user=username, password=passwd, database=dbname)
 # print('databse connected')
 
 # mysql://scott:tiger@localhost/mydatabase
 # ----------------------
-def scrapeEdmunds(year, make, model):
-    # URL to scrape from
-    url = 'https://www.edmunds.com/'+make+'/'+model+'/'+year+'/review/'
-    # url = 'https://www.edmunds.com/lexus/es-300/2001/review/'
-
-    # required to emulate broswer user agent
-    headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36',
-        'Origin': 'http://example.com',
-        'Referer': 'http://example.com/some_page'
-        }
-
-    # get html
-    source = requests.get(url, headers=headers).text
-    soup = BeautifulSoup(source, 'lxml')
-    imgSource = soup.findAll("img", {"class":"w-100"})
-
-    # Run Check
-    if len(imgSource) is 0:
-        imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
-    else:
-        imgSource = imgSource[0]['src']
-
-    # imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
-    # print(imgSource)
-    return imgSource
+# def scrapeEdmunds(year, make, model):
+#     # URL to scrape from
+#     url = 'https://www.edmunds.com/'+make+'/'+model+'/'+year+'/review/'
+#     # url = 'https://www.edmunds.com/lexus/es-300/2001/review/'
+#
+#     # required to emulate broswer user agent
+#     headers = {
+#         'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36',
+#         'Origin': 'http://example.com',
+#         'Referer': 'http://example.com/some_page'
+#         }
+#
+#     # get html
+#     source = requests.get(url, headers=headers).text
+#     soup = BeautifulSoup(source, 'lxml')
+#     imgSource = soup.findAll("img", {"class":"w-100"})
+#
+#     # Run Check
+#     if len(imgSource) is 0:
+#         imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
+#     else:
+#         imgSource = imgSource[0]['src']
+#
+#     # imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
+#     # print(imgSource)
+#     return imgSource
 # ----------------------
 def getHeader():
     # return a random user agent header based on generated number
