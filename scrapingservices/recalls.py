c5b7d5f0a41c21e0bf58db960b6399a554d8bf75
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-22 14:42:59 -0800

    major updates to recalls.py, trying to figure out multiprocessing module

--- scrapingservices/recalls.py
+++ scrapingservices/recalls.py
@@ -8,12 +8,63 @@ def getRecalls(year, make, model):
     #     print(item)
     # print( str(items['Results']) )
     # print(items)
-    return str(items['Results'])
+    return items['Results']
 
 # ----------------------
-# def handleFilesForRecalls(oldFile, newFile):
-#
-#
-#
-# # ----------------------
-# getRecalls('2002', 'acura','mdx')
+def row_operations(row, writer_object):
+    # We need to retrieve the list of recalls
+    recalls = getRecalls(row[0], row[1], row[2])
+    # Write what we already have + newly retrieved recalls item
+    writer_object.writerow([row[0], row[1], row[2], row[3], row[4], recalls])
+
+    return None
+# ----------------------
+def handleFilesForRecalls(oldFilePath, newFilePath):
+    # Does file reading & writing
+    with open(oldFilePath) as oldFile:
+        # create reader object from old file / path
+        reader = csv.reader(oldFile)
+
+        with open(newFilePath, 'w') as newFile:
+            # create writer object with new file / path
+            writer = csv.writer(newFile)
+            # write the headers on the first row of new file
+            writer.writerow(['year', 'make', 'model', 'body_style(s)', 'image_source(s)', 'recall(s)'])
+            # skip the first row of the old file
+            next(reader)
+
+            '''
+            This time, we have the following info {
+                year = row[0]
+                make = row[1]
+                model = row[2]
+                bodystyles = row[3]
+                image_sources = row[4]
+            }
+
+            Goal:
+                For each row, get its list of recalls as a json object
+                by making a call to getRecalls(year, make, model)
+
+            Results:
+                the CSV table should look like ->
+                ------------------------------------------------------------
+                year | make | model | body_styles | image_sources | recalls
+                ------------------------------------------------------------
+            '''
+            for row in reader:
+                ''' we have a 'reader obj', and a 'writer' obj that we instantiated inside here '''
+                row_operations(row, writer)
+                # # We need to retrieve the list of recalls
+                # recalls = getRecalls(row[0], row[1], row[2])
+                # # Write what we already have + newly retrieved recalls item
+                # writer.writerow([row[0], row[1], row[2], row[3], row[4], recalls])
+
+        return None
+# ----------------------
+def createOldPath(year):
+    return 'KBB_data/'+str(year)+'.csv'
+# ----------------------
+def createNewPath(year):
+    return str(year)+'.csv'
+# ----------------------
