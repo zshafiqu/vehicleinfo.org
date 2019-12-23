f2685bb4114fbd1e298a89533dd15b771992bbe0
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-23 00:11:58 -0800

    updates, my plan is to omit the old csvs with the recalls for the sake of coherence rn

--- scrapingservices/styles.py
+++ scrapingservices/styles.py
@@ -5,11 +5,11 @@ import images
 def getSoup(url):
     # Entered getSoup method
     print('About to make request for data')
-    i = 0
+    iter = 0
 
     # Continually try to make this request until its successful
     while True:
-        if i is 15:
+        if iter is 15:
             # OR until 15 times, at which point stop wasting time and break
             page = ''
             break
@@ -21,7 +21,7 @@ def getSoup(url):
         except:
             # If the request fails, log it, and try again
             print('Fail on request #'+str(i)+', trying again')
-            i += 1
+            iter += 1
             continue
         break # Break out of while loop here on success of the try block
 
@@ -77,17 +77,19 @@ def getSoup(url):
     # i = 0
     # while i < len(attributes)
 
-    for heading in headings:
-        print(heading.text)
-        print('\n')
-
-    for attribute in attributes:
-        print(attribute.text)
-        print('\n')
+    # for heading in headings:
+    #     print(heading.text)
+    #     print('\n')
+    #
+    # for attribute in attributes:
+    #     print(attribute.text)
+    #     print('\n')
 
     outermap = dict()
 
     ''' 0 , 1 '''
+    i = 0
+    j = 0
 
     while i < len(headings):
         z = 0
@@ -96,20 +98,75 @@ def getSoup(url):
             if z > 7:
                 break
             else:
-                innermap[attributes[j].text] = attributes[j+1].text
+                innermap[str(attributes[j].text)] = str(attributes[j+1].text)
             j += 2
             z += 2
-        outermap[headings[i].text] = innermap
+        outermap[str(headings[i].text)] = innermap
         i += 1
 
-
+    print(outermap)
     # All style headings within h3 tags with class = 'css-lg2ecn-StyledHeading3-defaultStyles-h3 e1jv8h5t2'
 
     # <p class="css-35ezg3" is the key, ex) -> Fuel Economy
     # <p> that follows is the value
 
-    return None
+    return outermap
 # ----------------------
+# ----------------------
+def handleFilesForStyles(oldFilePath, newFilePath):
+    # Does file reading & writing
+    with open(oldFilePath) as oldFile:
+        # create reader object from old file / path
+        reader = csv.reader(oldFile)
+
+        with open(newFilePath, 'w') as newFile:
+            # create writer object with new file / path
+            writer = csv.writer(newFile)
+            # write the headers on the first row of new file
+            writer.writerow(['year', 'make', 'model', 'body_style(s)', 'image_source(s)', 'recall(s)'])
+            # skip the first row of the old file
+            next(reader)
+
+            '''
+            This time, we have the following info {
+                year = row[0]
+                make = row[1]
+                model = row[2]
+                bodystyles = row[3]
+                image_sources = row[4]
+            }
+
+            Goal:
+                For each row, get its list of recalls as a json object
+                by making a call to getRecalls(year, make, model)
+
+            Results:
+                the CSV table should look like ->
+                ------------------------------------------------------------
+                year | make | model | body_styles | image_sources | recalls
+                ------------------------------------------------------------
+            '''
+            # jobs = []
+            for row in reader:
+                ''' we have a 'reader obj', and a 'writer' obj that we instantiated inside here '''
+                # p = multiprocessing.Process(target=row_operations, args=(row, writer))
+                # jobs.append(p)
+                # p.start()
+                row_operations(row, writer)
+                # # We need to retrieve the list of recalls
+                # recalls = getRecalls(row[0], row[1], row[2])
+                # # Write what we already have + newly retrieved recalls item
+                # writer.writerow([row[0], row[1], row[2], row[3], row[4], recalls])
+            # After row object completes
+            # bunches contains our data
+
+        return None
+# ----------------------
+def createOldPath(year):
+    return 'master_data/'+str(year)+'.csv'
+# ----------------------
+def createNewPath(year):
+    return str(year)+'.csv'
 # all info boxes are <div> tags with class = css-130z0y1-StyledBox-default emf8dez0
-url = 'https://www.kbb.com/bmw/3-series/1992/'
-getSoup(url)
+# url = 'https://www.kbb.com/bmw/3-series/1992/'
+# getSoup(url)
