a7613c78bfd7dfc202c14e03b49b36292b49982b
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-28 17:25:51 -0800

    insertion into database seems to be working

--- services/push_to_db.py
+++ services/push_to_db.py
@@ -1,4 +1,5 @@
-import os, mysql.connector, csv
+import os, mysql.connector, csv, json, ast
+from flask import jsonify
 # USAGE: to create or update our database with information from any csv file
 # ----------------------
 # Establish connection to our database using info stored locally on machine
@@ -24,6 +25,13 @@ mydb = mysql.connector.connect(
 
 curr = mydb.cursor()
 
+curr.execute("SELECT * FROM 1992_vehicles WHERE make LIKE 'ACURA' AND model like 'LEGEND'")
+
+for x in curr:
+    # x = json.loads(str(x))
+    # x = ast.literal_eval(x)
+    # x = jsonify(x)
+    print(x)
 
 '''
 Create a table for cars of a specific year that looks like {
@@ -33,31 +41,60 @@ ID (int) | Year (int) | Make (text) | Model (text) | Body-Styles (text) | Trim-D
 
 }
 '''
-year = 1992
-
-filename = 'master_data/'+str(year)+'.csv'
-tableName = str(year)+'_vehicles'
-
-curr.execute("CREATE TABLE "+tableName+" (id INT, year INT, make TEXT, model TEXT, body_styles TEXT, trim_data VARCHAR(MAX), image_sources VARCHAR(MAX))")
-
-with open(filename) as file:
-    reader = csv.reader(file)
-    next(reader)
-
-    for row in reader:
-
-        # year = row[0]
-        # make = row[1]
-        # model = row[2]
-        # bodystyles = row[3]
-        # trim_data = row[4]
-        # image_sources = row[5]
-        sql = "INSERT INTO "+tableName+" (year, make, model, body_styles, trim_data, image_sources) VALUES (%s, %s, %s, %s, %s, %s)"
-        curr.execute(sql, row)
-
-        mydb.commit()
-        print(curr.rowcount, "record inserted.")
-
+# year = 1992
+#
+# filename = 'master_data/'+str(year)+'.csv'
+# tableName = str(year)+'_vehicles'
+#
+# curr.execute("DROP TABLE IF EXISTS "+tableName)
+#
+# curr.execute("CREATE TABLE "+tableName+" (id INT AUTO_INCREMENT PRIMARY KEY, year INT, make TEXT, model TEXT, body_styles TEXT, trim_data JSON, image_sources JSON)")
+#
+# with open(filename) as file:
+#     reader = csv.reader(file)
+#     next(reader)
+#
+#     for row in reader:
+#
+#         # year = row[0]
+#         # make = row[1]
+#         # model = row[2]
+#         # bodystyles = row[3]
+#         # trim_data = row[4]
+#         # image_sources = row[5]
+#         sql = "INSERT INTO "+tableName+" (year, make, model, body_styles, trim_data, image_sources) VALUES (%s, %s, %s, %s, %s, %s)"
+#
+#         list = []
+#         list.append(str(row[0]))
+#         list.append(str(row[1]))
+#         list.append(str(row[2]))
+#         list.append(str(row[3]))
+#
+#         # Single quoted ->
+#         trim_data = row[4]
+#         image_sources = row[5]
+#
+#         # Literal
+#         trim_data = ast.literal_eval(trim_data)
+#         image_sources = ast.literal_eval(image_sources)
+#
+#         # To json
+#         trim_data = json.dumps(trim_data)
+#         image_sources = json.dumps(image_sources)
+#
+#         list.append(trim_data)
+#         list.append(image_sources)
+#         # list[1] = row[1]
+#         # list[2] = row[2]
+#         # list[3] = row[3]
+#         # list[4] = json.loads(row[4])
+#         # list[5] = json.loads(row[5])
+#
+#         curr.execute(sql, list)
+#
+#         mydb.commit()
+#         print(row[0]+' '+row[1]+' '+row[2]+' being inserted.')
+#         print(curr.rowcount, "record inserted.")
 # ----------------------
 # def connect_to_db(host, port, user, password, database):
 #     # Establish connection to our database using info stored locally on machine
