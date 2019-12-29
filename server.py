43b63c5b10f141b9eb8fabc1b21aaf47dc7f88f4
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-28 22:35:24 -0800

    updates to server, issues with connecting to the database. will resolve in the morning

--- server.py
+++ server.py
@@ -1,37 +1,38 @@
 from flask import Flask, render_template, request
-from bs4 import BeautifulSoup
-from urllib import urlopen
-import requests, json
-'''
-requirements ->
-flask, bs4, lxml
-'''
+from flaskext.mysql import MySQL
+import requests, json, os, mysql.connector
 # ----------------------
-# Create flask app
+# Initialize flask app
 app = Flask(__name__)
 # ----------------------
-def scrapeEdmunds(year, make, model):
-    # URL to scrape from
-    # url = 'https://www.edmunds.com/'+make+'/'+model+'/'+year+'/review/'
-    # # url = 'https://www.edmunds.com/lexus/es-300/2001/review/'
-    # # required to emulate broswer user agent
-    # headers = {
-    #     'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.90 Safari/537.36',
-    #     'Origin': 'http://example.com',
-    #     'Referer': 'http://example.com/some_page'
-    #     }
-    # # get html
-    # source = requests.get(url, headers=headers).text
-    # soup = BeautifulSoup(source, 'lxml')
-    # imgSource = soup.findAll("img", {"class":"w-100"})
-    # if len(imgSource) is 0:
-    #     imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
-    # else:
-    #     imgSource = imgSource[0]['src']
-    # print(imgSource)
-    imgSource = 'https://www.autotechemporium.com/frontend/assets/images/placeholder/inventory-full-placeholder.png'
-    return imgSource
+# Get info stored locally on machine
+username = os.environ.get('DB_USER')
+dbname = os.environ.get('DB_NAME')
+passwd = os.environ.get('DB_PASS')
+host = os.environ.get('DB_SERVER')
+port = os.environ.get('DB_PORT')
 # ----------------------
+# Configure flask app with db connection
+# app.config['MYSQL_HOST'] = host
+# app.config['MYSQL_PORT'] = port
+# app.config['MYSQL_USER'] = username
+# app.config['MYSQL_PASSWORD'] = passwd
+# app.config['MYSQL_DB'] = dbname
+
+# Establish connection to our database using info stored locally on machine
+# db_object = mysql.connector.connect(
+#     host=host,
+#     port=port,
+#     user=username,
+#     password=passwd,
+#     database=dbname
+#     )
+# ----------------------
+# Bind app to db obj
+# mysql = MySQL()
+# mysql.init_app(app)
+# ----------------------
+
 # Make an API call to the NHTSA page for recall Information
 def getRecalls(year, make, model):
     # Build URL for call to JSON response
@@ -41,26 +42,38 @@ def getRecalls(year, make, model):
 
     return items
 # ----------------------
-@app.route('/handleRequest', methods=['POST'])
-def process():
-    query = request.form['searchparameter']
-    parameter = query.split()
-    year = parameter[0]
-    make = parameter[1]
-    model = parameter[2]
-
-    recalls = getRecalls(year, make, model)
-    imagesrc = scrapeEdmunds(year, make, model)
-    return render_template('dashboard.html', recalls=recalls, imagesrc=imagesrc)
+# @app.route('/handleRequest', methods=['POST'])
+# def process():
+#     query = request.form['searchparameter']
+#     parameter = query.split()
+#     year = parameter[0]
+#     make = parameter[1]
+#     model = parameter[2]
+#
+#     recalls = getRecalls(year, make, model)
+#     # imagesrc = scrapeEdmunds(year, make, model)
+#
+#     # return render_template('dashboard.html', recalls=recalls)
+#     return recalls
 # ----------------------
 @app.route('/')
 def index():
     return render_template('home.html')
 # ----------------------
-@app.route('/jsonTest')
+# @app.route('/jsonTest')
+# def random():
+#     recalls = getRecalls('2007', 'lexus', 'es')
+#     return recalls
+# ----------------------
+@app.route('/api')
 def random():
-    recalls = getRecalls('2007', 'lexus', 'es')
-    return recalls
+    # recalls = getRecalls('2003', 'chevrolet', 'corvette')
+    cur = db_object.cursor()
+    cur.execute("SELECT * FROM 1992_vehicles WHERE make LIKE 'ACURA'")
+    list = []
+    for row in cur:
+        list.append(row)
+    return list
 # ----------------------
 if __name__ == '__main__':
     app.run(debug=True)
