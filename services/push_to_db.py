c7fd863bf8098ba15c1cc1100d4e5517d2b954e7
Zain Shafique <zshafiqu@ucsc.edu>
2019-12-28 17:47:52 -0800

    significant refactoring to abstract code into logical functions

--- services/push_to_db.py
+++ services/push_to_db.py
@@ -1,3 +1,4 @@
+# ----------------------
 import os, mysql.connector, csv, json, ast
 from flask import jsonify
 # USAGE: to create or update our database with information from any csv file
@@ -8,139 +9,94 @@ dbname = os.environ.get('DB_NAME')
 passwd = os.environ.get('DB_PASS')
 server = os.environ.get('DB_SERVER')
 port = os.environ.get('DB_PORT')
-
-# print(username)
-# print(dbname)
-# print(passwd)
-# print(server)
-# print(port)
 # ----------------------
-mydb = mysql.connector.connect(
-    host=server,
-    port=port,
-    user=username,
-    password=passwd,
-    database=dbname
-    )
-
-curr = mydb.cursor()
-
-curr.execute("SELECT * FROM 1992_vehicles WHERE make LIKE 'ACURA' AND model like 'LEGEND'")
-
-for x in curr:
-    # x = json.loads(str(x))
-    # x = ast.literal_eval(x)
-    # x = jsonify(x)
-    print(x)
-
-'''
-Create a table for cars of a specific year that looks like {
------------------------------------------------------------------------------------------------------------------
-ID (int) | Year (int) | Make (text) | Model (text) | Body-Styles (text) | Trim-Data (JSON)| Images-Sources (JSON)
------------------------------------------------------------------------------------------------------------------
-
-}
-'''
-# year = 1992
-#
-# filename = 'master_data/'+str(year)+'.csv'
-# tableName = str(year)+'_vehicles'
-#
-# curr.execute("DROP TABLE IF EXISTS "+tableName)
-#
-# curr.execute("CREATE TABLE "+tableName+" (id INT AUTO_INCREMENT PRIMARY KEY, year INT, make TEXT, model TEXT, body_styles TEXT, trim_data JSON, image_sources JSON)")
-#
-# with open(filename) as file:
-#     reader = csv.reader(file)
-#     next(reader)
-#
-#     for row in reader:
-#
-#         # year = row[0]
-#         # make = row[1]
-#         # model = row[2]
-#         # bodystyles = row[3]
-#         # trim_data = row[4]
-#         # image_sources = row[5]
-#         sql = "INSERT INTO "+tableName+" (year, make, model, body_styles, trim_data, image_sources) VALUES (%s, %s, %s, %s, %s, %s)"
-#
-#         list = []
-#         list.append(str(row[0]))
-#         list.append(str(row[1]))
-#         list.append(str(row[2]))
-#         list.append(str(row[3]))
-#
-#         # Single quoted ->
-#         trim_data = row[4]
-#         image_sources = row[5]
-#
-#         # Literal
-#         trim_data = ast.literal_eval(trim_data)
-#         image_sources = ast.literal_eval(image_sources)
-#
-#         # To json
-#         trim_data = json.dumps(trim_data)
-#         image_sources = json.dumps(image_sources)
-#
-#         list.append(trim_data)
-#         list.append(image_sources)
-#         # list[1] = row[1]
-#         # list[2] = row[2]
-#         # list[3] = row[3]
-#         # list[4] = json.loads(row[4])
-#         # list[5] = json.loads(row[5])
-#
-#         curr.execute(sql, list)
-#
-#         mydb.commit()
-#         print(row[0]+' '+row[1]+' '+row[2]+' being inserted.')
-#         print(curr.rowcount, "record inserted.")
+def json_conversion(somestring):
+    # Literal
+    somestring = ast.literal_eval(somestring)
+    # To json
+    somestring = json.dumps(somestring)
+    # return
+    return somestring
 # ----------------------
-# def connect_to_db(host, port, user, password, database):
-#     # Establish connection to our database using info stored locally on machine
-#     db_object = mysql.connector.connect(
-#         host=host,
-#         port=port,
-#         user=user,
-#         password=password,
-#         database=database
-#         )
-#
-#     return db_object
-# ----------------------
-def go_thru_file(filename):
-    # Traverse a file
-    print(filename)
+def file_to_database(vehicles_year, database_object):
+    # Create a cursor object from the database_object
+    curr = database_object.cursor()
 
+    # Create a table name and define the file name & path (route)
+    fileroute = file_pathify(vehicles_year)
+    tablename = str(vehicles_year)+'_vehicles'
+
+    # Before doing anything, check to see if the table exists & drop it if so
+    curr.execute("DROP TABLE IF EXISTS "+tableName)
+
+    # Create the table otherwise, scheme follows:
+    # --------------------------------------------------------------------
+    # ID | Year | Make | Model | Body-Styles | Trim-Data | Images-Sources
+    # --------------------------------------------------------------------
+    curr.execute(
+        "CREATE TABLE "+tableName+" (id INT AUTO_INCREMENT PRIMARY KEY, year INT, make TEXT, model TEXT, body_styles TEXT, trim_data JSON, image_sources JSON)"
+        )
+
+    # Traverse the file to do the operations on it
     with open(filename) as file:
         reader = csv.reader(file)
         next(reader)
 
         for row in reader:
-            year = row[0]
-            make = row[1]
-            model = row[2]
-            bodystyles = row[3]
-            trim_data = row[4]
-            image_sources = row[5]
 
-            # print(year+' '+make+' '+model+' '+bodystyles+' '+trim_data+' '+image_sources+'\n')
+            sql = "INSERT INTO "+tableName+" (year, make, model, body_styles, trim_data, image_sources) VALUES (%s, %s, %s, %s, %s, %s)"
+
+            # json_conversion ->
+            trim_data = json_conversion(row[4])
+            image_sources = json_conversion(row[5])
+
+            # Values list
+            list = []
+            list.append(str(row[0])) # Year
+            list.append(str(row[1])) # Make
+            list.append(str(row[2])) # Model
+            list.append(str(row[3])) # Body_styles
+            list.append(trim_data) # Trim_data
+            list.append(image_sources) # Image_sources
+
+            # Execute query
+            print(row[0]+' '+row[1]+' '+row[2]+' being inserted.')
+            curr.execute(sql, list)
+
+            # Commit change
+            database_object.commit()
+            print(curr.rowcount, "record inserted.")
 
     return None
+# ----------------------
+def connect_to_db(host, port, user, password, database):
+    # Establish connection to our database using info stored locally on machine
+    db_object = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        database=database
+        )
+
+    return db_object
 # ----------------------
 def file_pathify(year):
     # Return file path for given year
     return 'master_data/'+str(year)+'.csv'
 # ----------------------
-def print_range(min, max):
-    # Print range b/w HI & LOW
+def patch_range(min, max):
+    # Connect with global variables
+    mydb = connect_to_db(server, port, username, passwd, dbname)
+    # Go thru range b/w HI & LOW
     counter = min
+    
     while counter <= max:
         name = file_pathify(counter)
         print('BEGINNING FILE: '+name+'\n')
-        go_thru_file(name)
+        # Call the function to get the file & upload it to the database
+        file_to_database(counter, mydb)
         print('FINISHED FILE: '+name+'\n')
 
         counter += 1
 # ----------------------
-# print_range(1992, 1992)
