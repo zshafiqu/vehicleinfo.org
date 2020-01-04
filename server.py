c541ece3cda93a32843fde14f254609dcf56f4e4
Zain Shafique <zshafiqu@ucsc.edu>
2020-01-04 14:01:18 -0800

    get_app and run_app added

--- server.py
+++ server.py
@@ -210,5 +210,13 @@ def about():
     return render_template('about.html', page=page)
     # return render_template('temp.html')
 # ----------------------
-if __name__ == '__main__':
+def get_app():
+    return app
+# ----------------------
+def run_app(app):
     app.run()
+    return None
+# ----------------------
+run_app(app)
+# if __name__ == '__main__':
+#     app.run()
