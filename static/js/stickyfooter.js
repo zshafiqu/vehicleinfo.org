e05b307ab18de25e6a63e22a392a14f4be7cd3f6
Zain Shafique <zshafiqu@ucsc.edu>
2020-04-09 21:04:49 -0700

    move stickyfooter code to its own file

---
+++ static/js/stickyfooter.js
@@ -0,0 +1,64 @@
+// ------------------------------------------
+// Return total viewport size, aka the screen size
+function getViewPortHeight() {
+  return Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+}
+// ------------------------------------------
+// Return how much HTML we're actually presenting
+function getBodyHeight() {
+  var body = document.body;
+  var height = body.offsetHeight;
+  return height;
+}
+// ------------------------------------------
+// Get the difference between screen size and what we're presenting
+function calculateOffset(viewportHeight, actualHeight) {
+  return viewportHeight - actualHeight;
+}
+// ------------------------------------------
+// Adds a 'push' to the last section on a page if it doesn't fill the viewport
+function updateOffset(difference) {
+  // console.log('In update offsetHeight')
+
+  // Make reference to body and retrieve all sections as a vector
+  var body = document.body;
+  var sections = body.getElementsByTagName('section');
+
+  // Retrieve last element from vector and log it's styling if it exists
+  var lastElement = sections[sections.length-1];
+  // Access computed style to see embedded CSS from class
+  style = getComputedStyle(lastElement)
+
+  // Now let's check and see if some value already exists
+  var existingPadding = String(style.paddingBottom);
+  if (existingPadding != "") {
+    // console.log('There was existing padding')
+    // This means we have some existing padding
+    // Parse existingPadding as a substring to remove the 'px'
+    existingPadding = existingPadding.substring(0,existingPadding.length-2);
+
+    // Cast to Integer then check its type to be sure
+    existingPadding = parseInt(existingPadding);
+    console.log(typeof existingPadding);
+
+    // Then add it to the 'difference' variable to account for it
+    difference = difference + existingPadding;
+  }
+
+  // Once we've checked for existing value, we can mve forward with updating the styling
+  differenceAsString = String(difference)+"px"; // Make a string out of it
+  // console.log('About to update padding with value: '+differenceAsString)
+  lastElement.style.paddingBottom = differenceAsString; // Apply styling
+}
+// ------------------------------------------
+var vw = getViewPortHeight();
+var realheight = getBodyHeight();
+
+// If content is less than the screen size in height, apply correction
+if (realheight < vw) {
+  // console.log('We have an overflow');
+  var diff = calculateOffset(vw, realheight);
+  // console.log('Overflow amount is '+ String(diff));
+  updateOffset(diff);
+}
+// ------------------------------------------
