c4d16f6a429aaebca87708dff7d00502b89a1fbf
Zain Shafique <zshafiqu@ucsc.edu>
2020-04-11 16:35:14 -0700

    add imports for configuration

--- config.py
+++ config.py
@@ -1,6 +1,21 @@
+
+# This file's purpose is to setup the configurations for our Flask server
+# Abstracting this because as the application gets more complex,
+# we get more and more spaghetti code, so this should help with organization
+# ----------------------
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_talisman import Talisman
+# from flask_wtf import FlaskForm
+# from wtforms import SelectField
+from sqlalchemy.pool import QueuePool
+from flask_minify import minify
+from flask_caching import Cache
+import os
+
 
 config = Flask(__name__)
+
 
 if __name__ == '__main__':
     config.run()
