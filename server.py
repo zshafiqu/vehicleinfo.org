f4f1df1105bb8537adf88bf4f2a055f51b49edda
Zain Shafique <zshafiqu@ucsc.edu>
2020-04-11 22:16:57 -0700

    revert to production server

--- server.py
+++ server.py
@@ -178,7 +178,6 @@ def not_found(e):
     return render_template('error.html')
 # ----------------------
 if __name__ == '__main__':
-    # from waitress import serve
-    # serve(app)
-    app.run(debug=True)
+    from waitress import serve
+    serve(app)
 # ----------------------
