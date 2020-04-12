2647184ad06f9a0dab3c52e8d04f97437c1556a5
Zain Shafique <zshafiqu@ucsc.edu>
2020-04-11 17:03:39 -0700

    server config works

--- server.py
+++ server.py
@@ -8,42 +8,42 @@ from flask_minify import minify
 from flask_caching import Cache
 import requests, json, os, ast, datetime
 # ----------------------
-# Activate virtual env with - source env/bin/activate
-# Initialize flask app, enable auto deploy from master branch for heroku
-app = Flask(__name__)
-# Setup simple cache instance configuration & Initialize it to app instance
-cache = Cache(config={'CACHE_TYPE': 'simple'})
-cache.init_app(app)
-# From flask_minify, wrap app around minify module to minify the HTML/CSS/JS responses
-minify(app=app, html=True, js=True, cssless=True)
-# Designate application URL routing to occur with or without a trailing slash
-# By default, all of the routes defined below exist without a trailing slash
-app.url_map.strict_slashes = False
-# Use Talisman to force any http:// prefixed requests to redirect to https://
-# Edit the CSP in order to serve CSS styles and JavaScript files
-Talisman(app, content_security_policy=None)
-app.secret_key = os.environ.get('KEY')
+# # Activate virtual env with - source env/bin/activate
+# # Initialize flask app, enable auto deploy from master branch for heroku
+# app = Flask(__name__)
+# # Setup simple cache instance configuration & Initialize it to app instance
+# cache = Cache(config={'CACHE_TYPE': 'simple'})
+# cache.init_app(app)
+# # From flask_minify, wrap app around minify module to minify the HTML/CSS/JS responses
+# minify(app=app, html=True, js=True, cssless=True)
+# # Designate application URL routing to occur with or without a trailing slash
+# # By default, all of the routes defined below exist without a trailing slash
+# app.url_map.strict_slashes = False
+# # Use Talisman to force any http:// prefixed requests to redirect to https://
+# # Edit the CSP in order to serve CSS styles and JavaScript files
+# Talisman(app, content_security_policy=None)
+# app.secret_key = os.environ.get('KEY')
+# # ----------------------
+# # Configure flask app with info stored locally within environment (locally or heroku)
+# # To install mysqlclient, needed to run brew install mysql
+# app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+# app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('BASE_URI')
+# # ----------------------
+# # Bind app to db obj
+# db = SQLAlchemy(app)
+# # ----------------------
+# # Was running into a timeouterror for mySQL on Heroku's clearDB instance.
+# # Referencing stackoverflow, "the only work around I could find for this by
+# # talking to the ClearDB people is to add in the pessimistic ping when creating the engine."
+# # Not ideal since its pings DB everytime before you do a query, but avoids 500 Internal Server Error
+# db = SQLAlchemy(engine_options={"pool_size": 10, "poolclass":QueuePool, "pool_pre_ping":True})
 # ----------------------
-# Configure flask app with info stored locally within environment (locally or heroku)
-# To install mysqlclient, needed to run brew install mysql
-app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('BASE_URI')
-# ----------------------
-# Bind app to db obj
-db = SQLAlchemy(app)
-# ----------------------
-# Was running into a timeouterror for mySQL on Heroku's clearDB instance.
-# Referencing stackoverflow, "the only work around I could find for this by
-# talking to the ClearDB people is to add in the pessimistic ping when creating the engine."
-# Not ideal since its pings DB everytime before you do a query, but avoids 500 Internal Server Error
-db = SQLAlchemy(engine_options={"pool_size": 10, "poolclass":QueuePool, "pool_pre_ping":True})
-# ----------------------
-# from config import create_server_instance
-#
-# server = create_server_instance()
-# app = server.app
-# cache = server.cache
-# db = server.db
+from config import create_server_instance
+
+server = create_server_instance()
+app = server.app
+cache = server.cache
+db = server.db
 # ----------------------
 ''' Template filter converter for JSON formatted time '''
 @app.template_filter('strftime')
@@ -210,8 +210,8 @@ def report():
         # list comprehension to create tuples with (value, label) given by resulting lists from function calls
         form.make.choices = [(make['value'], make['label']) for make in get_distinct_makes_for_year(1992).json['makes']]
         form.model.choices = [(model['value'], model['label']) for model in get_all_models_for_year(form.make.choices[0][0], 1992).json['models']]
-    except:
-        return render_template('error.html')
+    except Exception as e:
+        return not_found(e)
 
     # If POST request, that means client hit 'submit' and is requesting a report
     if request.method == "POST":
