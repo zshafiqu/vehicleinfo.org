181c35068aaf6a948c21b470bec0d277f9f2e164
Zain Shafique <zshafiqu@ucsc.edu>
2020-05-31 18:15:44 -0700

    test_styles merged into scrape_styles_from_web and deleted

--- web_services/scrape_styles_from_web.py
+++ web_services/scrape_styles_from_web.py
@@ -8,7 +8,7 @@ from bs4 import BeautifulSoup, SoupStrainer
 import requests, json, os, csv, ast, random
 from generate_header import get_header
 # ----------------------
-# This function makes an HTTP request to KBB to gather the page's HTML
+# This function makes an HTTP request to KBB to gather the page's HTML and convert it to a BeautifulSoup object
 def get_soup_from_url(url):
     # Entered get_soup_from_url method, create an iterator to keep track
     # of request attempt count
@@ -49,57 +49,80 @@ def get_soup_from_url(url):
     # Return the filtered soup object
     return soup
 # ----------------------
-def parse_soup_to_dictionary(soup):
+# This function takes a soup object, and converts it into a list based on all the inner texts from the HTML
+def convert_soup_to_text_list(soup):
+    items = []
+    for item in soup:
+        temp = item.text
+        items.append(temp)
+    return items
+# ----------------------
+# This method takes in a list of headings and attributes, and parses them into a dictionary where attributes are split by trim
+def mappify_headings_and_attributes_from_list(headings, attributes):
+    # We have a list of headings (the trims)
+    # and list of attributes (all attributes for all trims, we need to slice this properly)
     '''
-        NOTE:
-        - Every vehicle follows the same pattern with up to 4 attributes per
-        trim; { Fuel Economy, Seating, Horsepower, Engine Size }
+    An example of headings:
+        ['Dealer Home Service', 'Base Style', 'TTS', 'RS']
+        len() = 4
 
-        - To parse through the soup object properly, we need to consider the following;
-            1. The length of the headings will tell us how many trims there are ( len(headings) = 3)
-            2. The length of the attributes is a function of ( 8 * len(headings) )
-            3. Attributes are split by a key, value mapping
-            4. Thus, len(attributes) = 24
-
-        - The implementation of parsing would be O( m*n ) for each trims
-        - You create a dictionary until you've reached counter = ( heading_number * 8 )
-
-        - See pseudocode below;
-            for heading in headings:
-                for attribute in attributes:
-                    go until you've finished this trim's attributes
-
-            dictionary : {
-                trim : { attributes },
-                trim : { attributes },
-                etc.
-            }
+    An example of attributes:
+        ['Combined Fuel Economy', '26 MPG', 'Seating', 'Seats 4', 'Horsepower', '228 @ 4500 RPM HP', 'Engine', '4-Cyl, Turbo, 2.0 Liter', 'Combined Fuel Economy', '25 MPG', 'Seating', 'Seats 4', 'Horsepower', '288 @ 5400 RPM HP', 'Engine', '4-Cyl, Turbo, 2.0 Liter', 'Combined Fuel Economy', '23 MPG', 'Seating', 'Seats 4', 'Horsepower', '394 hp HP', 'Engine', '5-Cyl, Turbo, 2.5 Liter']
+        len() = 24
     '''
-    headings = soup.findAll("h3")
-    attributes = soup.findAll("p")
-    outer_map = dict()
 
-    i = 0
-    j = 0
+    # Use 'i' to traverse the list of headings, remember KBB added this 'Dealer Home Service' box, so be sure to skip that
+    i = 1 # 'Dealer Home Service' will always be first, so just set that to 1 so we skip it
+    j = 0 # Use 'j' as a loop control variable for the attributes list
+    outer_map = dict() # This will be the wrapping dictionary for our JSON object
 
+    # For each trim
     while i < len(headings):
-        z = 0
-        inner_map = dict()
 
-        while j < len(attributes):
-            if z > 7:
+        topmost_attribute = attributes[0] # We need to know when we've finished a trim, so we use the topmost attribute as an indicator of when we've completed one trim
+        inner_map = dict() # Each trim gets its own dictionary with its attributes
+        new_trim_flag = True
+
+        # Go through the list of attributes until the second to last value
+        while j < len(attributes)-1:
+            # Check if the current attribute is the topmost attribute, because that would indicate we just finished a trim and are on the next set of attributes
+            if attributes[j] == topmost_attribute and new_trim_flag == False:
+                # Set thew new_trim_flag to True indicating that we're ready to work on a new trim
+                new_trim_flag = True
                 break
             else:
-                inner_map[str(attributes[j].text)] = str(attributes[j+1].text)
+                # Set the new_trim_flag to false cause we're currently workign on a trim
+                new_trim_flag = False
+                # Key, value mapping ... 'Combined Fuel Economy' : '26 MPG'
+                inner_map[attributes[j]] = attributes[j+1]
+                 # Jump by two because we've mapped the last two values as key,value
+                j += 2
 
-            j += 2
-            z += 2
-
-        outer_map[str(headings[i].text)] = inner_map
+        # Once the attributes have been assigned as we've broke out of the inner loop, map the trim to it's dictionary of attributes
+        outer_map[headings[i]] = inner_map
+        # Go to the next heading (trim)
         i += 1
 
+    # By now, all attributes and trims should be parsed as the dictionary 'trims'
     return outer_map
 # ----------------------
+# Dispatcher method that takes in some soup and converts it into a dictionary
+def parse_soup_to_dictionary(soup):
+    # Grab all the headings and attributes that we need
+    headings = soup.findAll('h3')
+    attributes = soup.findAll('p')
+
+    # Convert the headings and attributes to a list
+    headings_list = convert_soup_to_text_list(headings)
+    attributes_list = convert_soup_to_text_list(attributes)
+
+    # Pass to mappify function that'll handle conversion to JSON dictionary
+    dictionary = mappify_headings_and_attributes_from_list(headings_list, attributes_list)
+
+    # Return the JSON ready dict
+    return dictionary
+# ----------------------
+# Entry point for YEAR / MAKE / MODEL / BODYSTYLES
 def scrape_styles_data(year, make, model, bodystyles):
     # Cleanse model input
     model = model.replace('&', '')
@@ -142,12 +165,15 @@ def scrape_styles_data(year, make, model, bodystyles):
     # Return trims dictionary with data
     return trims
 # ----------------------
+# Gets filepath for source CSVs
 def get_source_filepath(year):
     return 'models_added/'+str(year)+'.csv'
 # ----------------------
+# Gets filepath for destination output CSVs
 def get_destination_filepath(year):
     return 'styles_added/'+str(year)+'.csv'
 # ----------------------
+# Creates directory for new output files
 def create_updated_csv_directory():
     import os, pathlib
     curr_path = str(pathlib.Path().absolute())
@@ -157,26 +183,7 @@ def create_updated_csv_directory():
     except OSError:
         print('Failed to create directory '+new_path)
 # ----------------------
-# def add_styles_for_new_vehicles(year):
-#     # Get source path to read from, and destination path to write to
-#     source_path = get_source_filepath(year)
-#     # destination_path = get_destination_filepath(year)
-#
-#     #Open source file to find the vehicles that have blanks for trim_data
-#     with open(source_path) as source:
-#         # Get reader object, skip first row as its the header
-#         reader = csv.reader(source)
-#         next(reader)
-#
-#         for row in reader:
-#             try:
-#                 temp = row[4]
-#             except:
-#                 print(row)
-#             # if row[3] is None:
-#             #     print(row)
-#             # print(row[3])
-# ----------------------
+# Row Dispatcher for each row in CSV
 def row_dispatcher(row):
     year = row[0]
     model = row[2]
@@ -198,6 +205,7 @@ def row_dispatcher(row):
     # Return results
     return styles_data
 # ----------------------
+# Opens source CSVs, parses them, makes call to get new data, then writes the new data
 def add_styles_for_new_vehicles(year):
     # Get source path to read from, and destination path to write to
     source_path = get_source_filepath(year)
@@ -233,3 +241,55 @@ def add_styles_for_new_vehicles(year):
 # ----------------------
 create_updated_csv_directory()
 add_styles_for_new_vehicles(2020)
+# ----------------------
+''' NOTE : BELOW IS THE OLD METHOD FOR PARSING SOUP TO A DICTIONARY '''
+# def parse_soup_to_dictionary(soup):
+#     '''
+#         - Every vehicle follows the same pattern with up to 4 attributes per
+#         trim; { Fuel Economy, Seating, Horsepower, Engine Size }
+#
+#         - To parse through the soup object properly, we need to consider the following;
+#             1. The length of the headings will tell us how many trims there are ( len(headings) = 3)
+#             2. The length of the attributes is a function of ( 8 * len(headings) )
+#             3. Attributes are split by a key, value mapping
+#             4. Thus, len(attributes) = 24
+#
+#         - The implementation of parsing would be O( m*n ) for each trims
+#         - You create a dictionary until you've reached counter = ( heading_number * 8 )
+#
+#         - See pseudocode below;
+#             for heading in headings:
+#                 for attribute in attributes:
+#                     go until you've finished this trim's attributes
+#
+#             dictionary : {
+#                 trim : { attributes },
+#                 trim : { attributes },
+#                 etc.
+#             }
+#     '''
+#     headings = soup.findAll("h3")
+#     attributes = soup.findAll("p")
+#     outer_map = dict()
+#
+#     i = 0
+#     j = 0
+#
+#     while i < len(headings):
+#         z = 0
+#         inner_map = dict()
+#
+#         while j < len(attributes):
+#             if z > 7:
+#                 break
+#             else:
+#                 inner_map[str(attributes[j].text)] = str(attributes[j+1].text)
+#
+#             j += 2
+#             z += 2
+#
+#         outer_map[str(headings[i].text)] = inner_map
+#         i += 1
+#
+#     return outer_map
+# # ----------------------
