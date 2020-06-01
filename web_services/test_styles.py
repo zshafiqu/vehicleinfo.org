89898fa064e7535adb40fac8abea7c08272030f3
Zain Shafique <zshafiqu@ucsc.edu>
2020-05-31 17:57:58 -0700

    getting ready to merge the test styles page into the scrape styles script

--- web_services/test_styles.py
+++ web_services/test_styles.py
@@ -120,14 +120,13 @@ def mappify_headings_and_attributes_from_list(headings, attributes):
     # Use 'i' to traverse the list of headings, remember KBB added this 'Dealer Home Service' box, so be sure to skip that
     i = 1 # 'Dealer Home Service' will always be first, so just set that to 1 so we skip it
     j = 0 # Use 'j' as a loop control variable for the attributes list
-    trims = dict() # This will be the wrapping dictionary for our JSON object
+    outer_map = dict() # This will be the wrapping dictionary for our JSON object
 
     # For each trim
     while i < len(headings):
 
-        curr_trim = headings[i] # Current trim
         topmost_attribute = attributes[0] # We need to know when we've finished a trim, so we use the topmost attribute as an indicator of when we've completed one trim
-        attributes_for_curr_trim = dict() # Each trim gets its own dictionary with its attributes
+        inner_map = dict() # Each trim gets its own dictionary with its attributes
         new_trim_flag = True
 
         # Go through the list of attributes until the second to last value
@@ -141,17 +140,17 @@ def mappify_headings_and_attributes_from_list(headings, attributes):
                 # Set the new_trim_flag to false cause we're currently workign on a trim
                 new_trim_flag = False
                 # Key, value mapping ... 'Combined Fuel Economy' : '26 MPG'
-                attributes_for_curr_trim[str(attributes[j])] = str(attributes[j+1])
+                inner_map[attributes[j]] = attributes[j+1]
                  # Jump by two because we've mapped the last two values as key,value
                 j += 2
 
         # Once the attributes have been assigned as we've broke out of the inner loop, map the trim to it's dictionary of attributes
-        trims[str(curr_trim)] = attributes_for_curr_trim
+        outer_map[headings[i]] = inner_map
         # Go to the next heading (trim)
         i += 1
 
     # By now, all attributes and trims should be parsed as the dictionary 'trims'
-    return trims
+    return outer_map
 # ----------------------
 # print()
 year = '2020'
