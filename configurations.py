fd12d78e4d0413bd5cf7b55ead42bdcb88d6372c
Zain Shafique <zshafiqu@ucsc.edu>
2020-07-03 16:55:26 -0700

    cache and db class methods merged

--- configurations.py
+++ configurations.py
@@ -47,105 +47,27 @@ class ServerObject:
 
         return app
     # ----------------------
+    def create_cache_instance(self):
+        # Setup simple cache instance configuration
+        cache = Cache(config={'CACHE_TYPE': 'simple'})
+        # Bind cache instance to app
+        cache.init_app(self.app)
 
+        return cache
+    # ----------------------
+    def create_db_cursor(self):
+        # Bind app to db obj
+        db = SQLAlchemy(self.app)
 
+        # Was running into a timeouterror for mySQL on Heroku's clearDB instance.
+        # Referencing stackoverflow, "the only work around I could find for this by
+        # talking to the ClearDB people is to add in the pessimistic ping when creating the engine."
+        # Not ideal since its pings DB everytime before you do a query, but avoids 500 Internal Server Error
+        db = SQLAlchemy(engine_options={"pool_size": 10, "poolclass":QueuePool, "pool_pre_ping":True})
 
+        return db
+    # ----------------------
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    
-    # Use this class to hold our app, it's cache object, and its database cursor
-    def __init__(self, app, cache, db):
-        self.app = app
-        self.cache = cache
-        self.db = db
-        self.cache_timeout = 1800 # Cache timeout is 30 minutes
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# ----------------------
-def create_cache_instance(app):
-    # Setup simple cache instance configuration & Initialize it to app instance
-    cache = Cache(config={'CACHE_TYPE': 'simple'})
-    cache.init_app(app)
-
-    return cache
-# ----------------------
-def create_db_cursor(app):
-    # Bind app to db obj
-    db = SQLAlchemy(app)
-    # Was running into a timeouterror for mySQL on Heroku's clearDB instance.
-    # Referencing stackoverflow, "the only work around I could find for this by
-    # talking to the ClearDB people is to add in the pessimistic ping when creating the engine."
-    # Not ideal since its pings DB everytime before you do a query, but avoids 500 Internal Server Error
-    db = SQLAlchemy(engine_options={"pool_size": 10, "poolclass":QueuePool, "pool_pre_ping":True})
-
-    return db
-# ----------------------
-def create_app():
-    # Activate virtual env with - source env/bin/activate
-    # Initialize flask app, enable auto deploy from master branch for heroku
-    app = Flask(__name__)
-
-    # From flask_minify, wrap app around minify module to minify the HTML/CSS/JS responses
-    minify(app=app, html=True, js=True, cssless=True)
-
-    # Designate application URL routing to occur with or without a trailing slash
-    # By default, all of the routes defined below exist without a trailing slash
-    app.url_map.strict_slashes = False
-
-    # Use Talisman to force any http:// prefixed requests to redirect to https://
-    # Edit the CSP in order to serve CSS styles and JavaScript files
-    Talisman(app, content_security_policy=None)
-    app.secret_key = os.environ.get('KEY')
-
-    # Configure flask app with info stored locally within environment (locally or heroku)
-    # To install mysqlclient, needed to run brew install mysql
-    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('BASE_URI')
-
-    return app
-# ----------------------
 def create_server_instance():
     # Use this method to create a configured application
     # Then return it as an instance of the server_object
