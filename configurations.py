623a70c00c486f3bc07711b658a6fce8430305a3
Zain Shafique <zshafiqu@ucsc.edu>
2020-07-03 16:46:23 -0700

    create app class made

--- configurations.py
+++ configurations.py
@@ -18,41 +18,62 @@ class ServerObject:
     db = None
     cache_timeout = None
 
+    # ----------------------
     # Object initializer method, takes in no parameters as the ServerObject's data is predefined above
     def __init__(self):
-        self.app =
-    # 
-    # # ServerObject's own method to create a Flask application object
-    # def create_app():
-    #     # Activate virtual env with - source env/bin/activate
-    #     # Initialize flask app, enable auto deploy from master branch for heroku
-    #     app = Flask(__name__)
-    #
-    #     # From flask_minify, wrap app around minify module to minify the HTML/CSS/JS responses
-    #     minify(app=app, html=True, js=True, cssless=True)
-    #
-    #     # Designate application URL routing to occur with or without a trailing slash
-    #     # By default, all of the routes defined below exist without a trailing slash
-    #     app.url_map.strict_slashes = False
-    #
-    #     # Use Talisman to force any http:// prefixed requests to redirect to https://
-    #     # Edit the CSP in order to serve CSS styles and JavaScript files
-    #     Talisman(app, content_security_policy=None)
-    #     app.secret_key = os.environ.get('KEY')
-    #
-    #     # Configure flask app with info stored locally within environment (locally or heroku)
-    #     # To install mysqlclient, needed to run brew install mysql
-    #     app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
-    #     app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('BASE_URI')
-    #
-    #     return app
-    #
-    # # Use this class to hold our app, it's cache object, and its database cursor
-    # def __init__(self, app, cache, db):
-    #     self.app = app
-    #     self.cache = cache
-    #     self.db = db
-    #     self.cache_timeout = 1800 # Cache timeout is 30 minutes
+        pass
+    # ----------------------
+    # ServerObject's own method to create a Flask application object, as well as assign app configs
+    def create_app(self):
+        # Create Flask app object
+        app = Flask(__name__)
+
+        # From flask_minify, wrap app around minify module to minify the HTML/CSS/JS responses
+        minify(app=app, html=True, js=True, cssless=True)
+
+        # Designate application URL routing to occur with or without a trailing slash
+        # By default, all of the routes defined below exist without a trailing slash
+        app.url_map.strict_slashes = False
+
+        # Use Talisman to force any http:// prefixed requests to redirect to https://
+        # Edit the CSP in order to serve CSS styles and JavaScript files
+        Talisman(app, content_security_policy=None)
+        app.secret_key = os.environ.get('KEY')
+
+        # Configure flask app with info stored locally within environment (locally or heroku)
+        # To install mysqlclient, needed to run brew install mysql
+        app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+        app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('BASE_URI')
+
+        return app
+    # ----------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
+    # Use this class to hold our app, it's cache object, and its database cursor
+    def __init__(self, app, cache, db):
+        self.app = app
+        self.cache = cache
+        self.db = db
+        self.cache_timeout = 1800 # Cache timeout is 30 minutes
 
 
 
