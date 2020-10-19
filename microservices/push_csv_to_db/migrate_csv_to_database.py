7f9642570ca80ff7446a6a2ec03248a18ffa4951
Zain Shafique <zshafiqu@ucsc.edu>
2020-10-19 14:19:22 -0700

    Some modifications to the database script

--- microservices/push_csv_to_db/migrate_csv_to_database.py
+++ microservices/push_csv_to_db/migrate_csv_to_database.py
@@ -15,6 +15,7 @@ def get_database_object():
         port=port,
         user=user,
         password=password,
+        database=database
         )
 
     return db_object
@@ -77,10 +78,24 @@ def write_csv_in_range():
     start_year = int(input("Enter the start year you'd like to write to the database: "))
     end_year = int(input("Enter the end year you'd like to to write to the database: "))
 
+    # Needed since no guarantee the table exists
+    host = os.environ.get('RDS_HOST')
+    port = os.environ.get('RDS_PORT')
+    user = os.environ.get('RDS_USER')
+    password = os.environ.get('RDS_PASSWORD')
+
+    # Connect to DB without specifying a database
+    db_object = mysql.connector.connect(
+        host=host,
+        port=port,
+        user=user,
+        password=password,
+        )
+
+    # Use this local object to create the database
     initial_sql = "CREATE DATABASE IF NOT EXISTS `vehicleinfo-db`" 
-    db_object = get_database_object()
-    curr = db_object.cursor()
-    curr.execute(initial_sql)
+    curr = db_object.cursor() # Get the cursor 
+    curr.execute(initial_sql)  # Execute the initial DB creation 
 
     while start_year <= end_year:
         print('*******************************************************')
